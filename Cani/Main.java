f24297846ec8516d55ffa8e11bddca3b9bc0b16d
Kuro-no-Kenshi <dani04ranzo@gmail.com>
2022-05-14 19:38:58 +0200

    Update Main.java

--- Cani/Main.java
+++ Cani/Main.java
@@ -1,14 +1,23 @@
 import java.util.*;
 import java.io.*;
+import java.text.ParseException;
+import java.time.*;
 
 public class Main {
     public static void main(String[] args) {
         String nome;
         String razza;
-        int g, m, a;
+        int g, m, a, s;
         LocalDate dataN;
+      try{
+        File f=new File("Canile.txt");
+        
+				FileWriter scrivo=new FileWriter(f);
 
         Scanner t=new Scanner(System.in);
+        Scanner leggo=new Scanner(f);
+        String riga;
+        
         ArrayList <Cane> c1=new ArrayList <Cane>();
 
         do{
@@ -31,14 +40,17 @@ public class Main {
                 a=t.nextInt();
                 dataN=LocalDate.of(a,m,g);
 
-                c1.add(nome, razza, dataN);
+                c1.add(new Cane(nome, razza, dataN));
+
+                scrivo.write(nome+"*"+razza+"*"+dataN+"\n");
+                scrivo.close();
                 break;
 
                 case 2:
                 output("Inserire il nome del cane dal eliminare adl file: ");
                 String cerca=t.next();
-                for(int i=0;i<c1.size;i++){
-                    if(cerca.equals(c1.get(i).nome)){
+                for(int i=0;i<c1.size();i++){
+                    if(cerca.equals(c1.get(i).getNome())){
                         output("Il cane è stato trovato! ");
                         c1.remove(i);
                     }
@@ -46,16 +58,29 @@ public class Main {
                 break;
 
                 case 3:
-                output("Inserire l'anno: ");
-                int anno=t.nextInt();
-                for(int i=0;i<c1.size;i++){
-                    if(dataN(isAfter)){
-                        
+                output("Inserire l'anno:");
+                LocalDate anno=t.nextInt();
+                for(int i=0;i<c1.size();i++){
+                    if(dataN.isAfter(anno)){
+                        while(leggo.hasNextLine()){
+      						riga=leggo.nextLine();
+      						String[] elementi=riga.split(":");
+      						System.out.println("Materia "+elementi[0]);
+     					  	System.out.println("Valutazione "+elementi[1]);
+      						int valutazione=Integer.parseInt(elementi[1]);
+    					}
                     }
                 }
             }
         }while(s!=0);
 
+
+        
+      }catch(Exception e){
+        output("E' avvenuto un errore");
+      }
+
+        
     }
     public static void output(String s){
         System.out.println(s);
